8c492de28ecd0e76a6c2f092edd02f9cea3943c9
Maxim Kolesnik <makleso6@gmail.com>
2019-03-27 14:46:48 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/IBM-Swift/Kitura-net.git", from: "2.1.0"),
+        .package(url: "https://github.com/makleso6/Kitura-net.git", from: "2.1.8"),
         .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "1.0.0"),
 
     ],
